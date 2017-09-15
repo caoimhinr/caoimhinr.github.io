e3725bbc38b51070575195296d8163ef2dc5ce7b
caoimhinr <31962018+caoimhinr@users.noreply.github.com>
2017-09-15 16:58:12 +0200

    20170915

---
+++ SQliteManagement/SQliteManagement/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace SQliteManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboTables = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dtgData = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgData)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cboTables
+            // 
+            this.cboTables.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboTables.FormattingEnabled = true;
+            this.cboTables.Location = new System.Drawing.Point(12, 12);
+            this.cboTables.Name = "cboTables";
+            this.cboTables.Size = new System.Drawing.Size(789, 24);
+            this.cboTables.TabIndex = 0;
+            this.cboTables.SelectedValueChanged += new System.EventHandler(this.cboTables_SelectedValueChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(807, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(133, 24);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dtgData
+            // 
+            this.dtgData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtgData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgData.Location = new System.Drawing.Point(12, 78);
+            this.dtgData.Name = "dtgData";
+            this.dtgData.RowTemplate.Height = 24;
+            this.dtgData.Size = new System.Drawing.Size(928, 389);
+            this.dtgData.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.cboTables);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(928, 50);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 479);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dtgData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgData)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboTables;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dtgData;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ SQliteManagement/SQliteManagement/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQliteManagement
+{
+    public partial class Form1 : Form
+    {
+        private bool _updating;
+
+        public Form1()
+        {
+            InitializeComponent();
+            var connection = new SQLiteConnection("Data Source=tower.db");
+            connection.Open();
+            var statement = @"SELECT name FROM sqlite_master WHERE type='table';";
+            var command = new SQLiteCommand(statement, connection);
+            var adapter = new SQLiteDataAdapter(command);
+            var table = new DataTable();
+            adapter.Fill(table);
+            cboTables.Items.Clear();
+            foreach (DataRow row in table.Rows)
+            {
+                cboTables.Items.Add(row["name"].ToString());
+            }
+        }
+
+        private void cboTables_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cboTables.SelectedItem != null)
+            {
+                var tableName = cboTables.SelectedItem.ToString();
+                using (var connection = new SQLiteConnection("Data Source=tower.db"))
+                {
+                    connection.Open();
+                    var statement = String.Format(@"SELECT * FROM {0};", tableName);
+                    var command = new SQLiteCommand(statement, connection);
+                    var adapter = new SQLiteDataAdapter(command);
+                    var table = new DataTable(tableName);
+                    adapter.Fill(table);
+                    _updating = true;
+                    dtgData.DataSource = table.DefaultView;
+                    _updating = false;
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (var connection = new SQLiteConnection("Data Source=tower.db"))
+            {
+                connection.Open();
+                var view = (DataView)dtgData.DataSource;
+                var table = view.Table.GetChanges();
+                var statement = @"UPDATE " + table.TableName + " SET ";
+                foreach (DataColumn column in table.Columns)
+                {
+                    if (!column.ColumnName.Equals("Id"))
+                        statement += column.ColumnName + "=@" + column.ColumnName + ",";
+                }
+                statement = statement.Substring(0, statement.Length - 1) + " WHERE Id=@Id";
+
+                var command = new SQLiteCommand(statement, connection);
+                foreach (DataRow row in table.Rows)
+                {
+                    command.Parameters.Clear();
+                    foreach (DataColumn column in table.Columns)
+                    {
+                        command.Parameters.AddWithValue("@" + column.ColumnName, row[column.ColumnName]);
+                    }
+                    var result = command.ExecuteNonQuery();
+                    if (result == 0)
+                    {
+                        statement = @"INSERT INTO " + table.TableName + " VALUES (";
+                        command = new SQLiteCommand(connection);
+                        foreach (DataColumn column in table.Columns)
+                        {
+                            statement += "@" + column.ColumnName + ",";
+                            command.Parameters.AddWithValue("@" + column.ColumnName, row[column.ColumnName]);
+                        }
+                        statement = statement.Substring(0, statement.Length - 1) + ")";
+                        command.CommandText = statement;
+                        command.ExecuteNonQuery();
+                    }
+                }
+                view.Table.AcceptChanges();
+            }
+        }
+    }
+}
