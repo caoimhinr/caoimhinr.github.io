324888ad1ed653ce493287a24407b1641334c524
caoimhinr <31962018+caoimhinr@users.noreply.github.com>
2018-02-19 14:17:26 +0100

    players

--- js/titan.js
+++ js/titan.js
@@ -7,11 +7,13 @@ function Player() {
 
 function Stat(){
 	return {
-		Element: "",
-		Values: []
+		Level: 0,
+		Damage: 0,
+		Titan : {}
 	};
 }
 
+var currentPlayer = new Player();
 var players = [];
 var titans = (function() {
         var json = null;
@@ -31,24 +33,43 @@ var titans = (function() {
 	$(document).ready(function() {
 		$('#titans').html('');
 		$.each(titans, function(i, e) {
-			$('#titans').append(GetTitanUI(e, 1));
+			$('#titans').append(GetTitanUI(null, e, 1));
 		});
 		
-		var player = new Player();
-		player.Name = "Caoimhinr";
+		$('#btnSave').click(function() {
+			var player = new Player();
+			player.Name = $('#playerName').val();
+			
+			$.each(titans, function(i, e) {
+				var damage = $('#titan' + e.Level).val();
+				if (damage > 0) {
+					var stat = new Stat();
+					stat.Level = e.Level;
+					stat.Damage = damage;
+					stat.Titan = e;
+					player.Stats.push(stat);
+				}
+			});
+			
+			players.push(player);
+			RefreshPlayerCombo();
+			console.log(player);
+		});
 		
-		var stat = new Stat();
-		stat.Element = "Water";
-		stat.Values = [
-		
-		];
 	});
 })();
 
-function GetTitanUI(item, id) {
+function GetTitanUI(stat, titan, id) {
 	var titanUI = $("<div class='row'><div class='col-md-1'>" + 
-						item.Level + "</div><div class='col-md-2'>" + 
-						item.Element + "</div><div class='col-md-3'>" + 
-						item.HP + "</div><div class='col-md-2'>Damage:</div><input type='text' id='playerDamage' class='col-md-4' /></div>");
+						titan.Level + "</div><div class='col-md-1'>" + 
+						titan.Element + "</div><div class='col-md-2'>" + 
+						titan.HP + "</div><div class='col-md-5'>Damage - Current: " + 10000000 + "</div><input type='text' id='titan" + titan.Level + "' class='col-md-3' value='0' /></div>");
 	return titanUI;
+}
+
+function RefreshPlayerCombo() {
+	$.each(players, function(i, e) {
+		$('#cboPlayers').html('');
+		$('#cboPlayers').append("<option>" + e.Name + "</option>");
+	});
 }
